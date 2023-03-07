0f5c4969927fcc9ccf6ba7e19f16f722c24008d4
Prantik <prantiknoor@gmail.com>
2023-03-07 17:54:31 +0600

    feat(extension.ts): info message updated

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('hello-vscode-extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from hello-vscode-extension!');
+		vscode.window.showInformationMessage('Hello World from hello-vscode-extension! (it is Prantik)');
 	});
 
 	context.subscriptions.push(disposable);
